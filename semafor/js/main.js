90efade9ad70b9cf2f57229b8e322f1d3db2aa5e
Dragos Tache <dragos.tache@interdigital.ro>
2023-04-20 20:24:35 +0300

    fixed to work

--- semafor/js/main.js
+++ semafor/js/main.js
@@ -1,30 +1,75 @@
 let lights = document.querySelectorAll('#isiqfor>div');
-let timers =[5000, 2000, 7000];
-let counter= 0;
+let timers = [5000, 2000, 7000];
+let counter = 0;
+let directionUp = true;
 let lights2 = document.querySelectorAll('#isiqfor2>div');
-let timers2 = [5000, 2000 , 7000];
 let counter2 = 2;
+let directionUp2 = false;
 
-window.addEventListener('load',start);
+var start = function () {
+    console.log('inside start');
+    setTimeout(function () {
+        traffic();
+        traffic2();
+    }, 5000);
+}
 
-var start = setTimeout(function(){
-    traffic();
-    traffic2();
-},5000);
-window.addEventListener('load',start);
+window.addEventListener('load', start);
+
 function traffic() {
-    for(i= 0; si <l ights.length; i++) {
-       lights[i].classList.add('on');
+    console.log('inside traffic');
+
+    lights[counter].classList.remove('on');
+    if(directionUp){
+        console.log('inside traffic - direction up');
+        if(counter == 2){
+            directionUp = false;
+            counter = 1;
+        } else {
+            counter++;
+        }
+    } else {
+        if(counter == 0){
+            directionUp = true;
+            counter = 1;
+        } else {
+            counter--;
+        }
+
     }
+    console.log(`inside traffic - counter: ${counter}`);
+
     lights[counter].classList.add('on');
+    console.log(`inside traffic - counter: ${counter}`);
     setTimeout(traffic, timers[counter]);
-    counter = (counter + 1) % timers.length;
 
 }
 
 function traffic2() {
-    for(i = 0; i < lights2.length; i++) {
-        lights[i].classList.remove('on')
+    console.log('inside traffic2');
+
+    lights2[counter2].classList.remove('on');
+    if(directionUp2){
+        console.log('inside traffic - direction up');
+        if(counter2 == 2){
+            directionUp2 = false;
+            counter2 = 1;
+        } else {
+            counter2++;
+        }
+    } else {
+        if(counter2 == 0){
+            directionUp2 = true;
+            counter2 = 1;
+        } else {
+            counter2--;
+        }
+
     }
+    console.log(`inside traffic2 - counter: ${counter2}`);
+
+    lights2[counter2].classList.add('on');
+    console.log(`inside traffic2 - counter: ${counter2}`);
+    setTimeout(traffic2, timers[counter2]);
 
 }
